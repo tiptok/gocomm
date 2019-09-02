d7424b91e8ba9da016505707058de3ba5794953b
tiptok <785409885@qq.com>
2019-09-02 17:22:27 +0800

    先注释掉

--- config/config.go
+++ config/config.go
@@ -1,55 +1,46 @@
 package config
 
 
-import (
-	"fmt"
-	"sync"
-
-	"github.com/micro/go-config"
-	"github.com/micro/go-config/source/grpc"
-	"github.com/micro/go-log"
-)
-
-var (
-	once sync.Once
-)
-
-func Init(addr, name string) {
-	once.Do(func() {
-		source := grpc.NewSource(
-			grpc.WithAddress(addr),
-			grpc.WithPath(name),
-		)
-
-		if err := config.Load(source); err != nil {
-			log.Fatal(err)
-			return
-		}
-
-		go func() {
-			watcher, err := config.Watch()
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			for {
-				v, err := watcher.Next()
-				if err != nil {
-					log.Log(err)
-					continue
-				}
-
-				log.Logf("[Init] file change: %v", string(v.Bytes()))
-			}
-		}()
-	})
-}
-
-func Get(conf interface{}, path ...string) (err error) {
-	if v := config.Get(path...); v != nil {
-		err = v.Scan(conf)
-	} else {
-		err = fmt.Errorf("[Get] 配置不存在, err: %v", path)
-	}
-	return
-}
+//var (
+//	once sync.Once
+//)
+//
+//func Init(addr, name string) {
+//	once.Do(func() {
+//		source := grpc.NewSource(
+//			grpc.WithAddress(addr),
+//			grpc.WithPath(name),
+//		)
+//
+//		if err := config.Load(source); err != nil {
+//			log.Fatal(err)
+//			return
+//		}
+//
+//		go func() {
+//			watcher, err := config.Watch()
+//			if err != nil {
+//				log.Fatal(err)
+//			}
+//
+//			for {
+//				v, err := watcher.Next()
+//				if err != nil {
+//					log.Log(err)
+//					continue
+//				}
+//
+//				log.Logf("[Init] file change: %v", string(v.Bytes()))
+//			}
+//		}()
+//	})
+//}
+//
+//func Get(conf interface{}, path ...string) (err error) {
+//	if v := config.Get(path...); v != nil {
+//		err = v.Scan(conf)
+//	} else {
+//		err = fmt.Errorf("[Get] 配置不存在, err: %v", path)
+//	}
+//	return
+//}
