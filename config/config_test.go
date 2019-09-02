97bac64fe0b3278316dfda9ba46e5afa83e7bb29
tiptok <785409885@qq.com>
2019-09-02 18:46:40 +0800

    config

--- config/config_test.go
+++ config/config_test.go
@@ -1,23 +1,23 @@
 package config
 
-//import (
-//	"fmt"
-//	"log"
-//	"testing"
-//)
-//
-//func TestInit(t *testing.T){
-//	var (
-//		App    App
-//		Mysql  Mysql
-//		Redis  Redis
-//		Consul Consul
-//	)
-//	Init("127.0.0.1:9600", "srv-passport")
-//	Get(&App, "app")
-//	Get(&Consul, "consul")
-//	Get(&Mysql, "mysql")
-//	Get(&Redis, "redis")
-//
-//	log.Println(fmt.Sprintf("%v",App))
-//}
+import (
+	"fmt"
+	"log"
+	"testing"
+)
+
+func TestInit(t *testing.T){
+	var (
+		App    App
+		Mysql  Mysql
+		Redis  Redis
+		Consul Consul
+	)
+	Init("127.0.0.1:9600", "srv-passport")
+	Get(&App, "app")
+	Get(&Consul, "consul")
+	Get(&Mysql, "mysql")
+	Get(&Redis, "redis")
+
+	log.Println(fmt.Sprintf("%v",App))
+}
